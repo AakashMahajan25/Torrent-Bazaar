f055b8667e9e8f36d4d4b42aad545eb62e4468e5
aakash <mahajanaakash108@gmail.com>
2024-07-27 23:09:28 +0530

    Constants Made

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
 import connectDB from "./config/db";
+import app from "./app";
+import { PORT } from "./constants/env";
+
+try {
+  connectDB().then(() => {
+    console.log("Database Connected Successfully!");
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  });
+} catch (error) {
+  console.log("Error connecting to Database: " + error);
+}
